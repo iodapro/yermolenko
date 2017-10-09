e5903baa3355c316423e8ffec1daf7fa1ada4edd
Mykhailo <mikhail.nikolajchuk@gmail.com>
2017-10-09 22:04:32 +0300

    added resizing updated testing

--- nikolaichuk/task2_cust_vect/my_dyn_array.h
+++ nikolaichuk/task2_cust_vect/my_dyn_array.h
@@ -6,9 +6,9 @@ using std::size_t;
 template <class elem> 
 class MyDynamicArray {
 private:
-    size_t size;        // size of data in array
+    size_t size;        // size of array in array
     size_t capacity;    // maximum current capacity
-    elem* data;           // pointer to actual items in memory
+    elem* array;           // pointer to actual items in memory
     
     const static int initial_size = 32;
     const static int size_multiply = 2;
@@ -28,27 +28,46 @@ MyDynamicArray<elem>::MyDynamicArray() {
     size = 0;
     capacity = initial_size;
 
-    data = new elem[capacity];
+    array = new elem[capacity];
 }
 
 template <class elem> 
 MyDynamicArray<elem>::~MyDynamicArray() {
-    if (data) {
-        delete[] data;
+    if (array) {
+        delete[] array;
     }
+}
+
+template <class elem>
+void MyDynamicArray<elem>::resize(){
+    capacity *= size_multiply;
+    elem *new_array = new elem[capacity];
+
+    for (size_t i=0;i<size;i++){
+        new_array[i] = array[i];
+    }
+
+    delete[] array;
+    array = new_array;
 }
 
 template <class elem>
 void MyDynamicArray<elem>::push_back(elem &item){
 // TODO: allow resize of dynamic array    
-    if (size < capacity)
-    data[size] = item;
+    if (size >= capacity) {
+        resize();
+    };
+
+    array[size] = item;
     ++size;
 }
 
 template <class elem>
 elem& MyDynamicArray<elem>::operator[] (size_t index) {
     if (index < size - 1) {
-        return data[index];
+        return array[index];
+    } else {
+        std::cout << "Index out of range.\n";
+        // TODO: add exception throw
     }
 }
--- nikolaichuk/task2_cust_vect/test_my_array.cpp
+++ nikolaichuk/task2_cust_vect/test_my_array.cpp
@@ -5,12 +5,14 @@ int main(int argc, char const *argv[])
 {
     MyDynamicArray<int> arr;
 
-    for(int i;i<20;i++){
+    for(int i;i<40;i++){
         arr.push_back(i);
     };
 
     std::cout << arr[15] << std::endl;
     std::cout << arr[10] << std::endl;
+    std::cout << arr[38] << std::endl;
+    std::cout << arr[90] << std::endl;
 
     return 0;
 }
