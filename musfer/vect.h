9832ed92c84004233f110eddaa0a3f685e559952
Musfer <adzhymambetov@gmail.com>
2017-10-10 00:27:21 +0300

    fixed bugs(class working)

---
+++ musfer/vect.h
@@ -0,0 +1,17 @@
+#ifndef VECT_H
+#define VECT_H
+
+ 
+class vect
+{
+public:
+vect();
+int length();
+int *array;
+int capacity;
+int s;
+void push_back(int a);
+int *sarray;
+int size();	  	
+};
+#endif
